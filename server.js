979b61eb55430c163a488d5889eddc4f0276149b
rodrigoFM17 <rodrig4243@gmail.com>
2025-05-14 13:21:55 -0600

    author updated

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(helmet());
 app.use(express.json());
 
 app.get('/health', (_req, res) => {
-  res.status(200).json({ status: 'UP', timestamp: new Date().toISOString(), author: 'Diego Eduardo Bejar Zea' });
+  res.status(200).json({ status: 'UP', timestamp: new Date().toISOString(), author: 'Diego Eduardo Bejar Zea DEV' });
 });
 
 const PORT = process.env.PORT || 3000;
